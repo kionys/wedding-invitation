bc042f16d30381befe0485f300b81a96e8f05287
kionys <kiwon7271@naver.com>
2024-09-03 02:04:18 +0900

    update: video 컴포넌트 video경로 및 옵션 적용

--- src/components/sections/video.tsx
+++ src/components/sections/video.tsx
@@ -5,6 +5,18 @@ import styles from './video.module.scss';
 const cx = classNames.bind(styles);
 
 const Video = () => {
-  return <Section>Video</Section>;
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={false}
+        muted={true}
+        loop={true}
+        controls={true}
+        poster={'/assets/images/wedding_07.jpg'}
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  );
 };
 export default Video;
