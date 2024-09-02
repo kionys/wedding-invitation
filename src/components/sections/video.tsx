264f0f31525a372612712da3c1006b67bfaf20a2
kionys <kiwon7271@naver.com>
2024-09-03 01:37:55 +0900

    add: Video 컴포넌트, scss 추가

---
+++ src/components/sections/video.tsx
@@ -0,0 +1,10 @@
+import Section from '@shared/section';
+import classNames from 'classnames/bind';
+import styles from './video.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Video = () => {
+  return <Section>Video</Section>;
+};
+export default Video;
