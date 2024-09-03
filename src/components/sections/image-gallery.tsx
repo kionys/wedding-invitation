9eae594d62642bafcdcfb0a83d447531cf9912a9
kionys <kiwon7271@naver.com>
2024-09-03 11:45:26 +0900

    update: 이미지 갤러리 뷰어 이벤트 적용

--- src/components/sections/image-gallery.tsx
+++ src/components/sections/image-gallery.tsx
@@ -1,22 +1,47 @@
 import Section from '@shared/section';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
+import { ImageViewer } from '../image-viewer/image-viewer';
 import styles from './image-gallery.module.scss';
 
 const cx = classNames.bind(styles);
 
 const ImageGallery = ({ images }: { images: string[] }) => {
+  const [selectedIdx, setSelectedIdx] = useState(-1);
+
+  const open = selectedIdx > -1;
+
+  const onClickImageOpen = (idx: number) => {
+    setSelectedIdx(idx);
+  };
+
+  const onClickImageClose = () => {
+    setSelectedIdx(-1);
+  };
   return (
-    <Section className={cx('container')} title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, i) => {
-          return (
-            <li key={i} className={cx('wrap-image')}>
-              <img src={src} alt="사진첩 이미지" />
-            </li>
-          );
-        })}
-      </ul>
-    </Section>
+    <>
+      <Section className={cx('container')} title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, i) => {
+            return (
+              <li
+                key={i}
+                className={cx('wrap-image')}
+                onClick={() => onClickImageOpen(i)}
+              >
+                <img src={src} alt="사진첩 이미지" />
+              </li>
+            );
+          })}
+        </ul>
+      </Section>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={onClickImageClose}
+      />
+    </>
   );
 };
 export default ImageGallery;
