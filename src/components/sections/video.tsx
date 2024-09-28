8f452fb44a9aad27cf18bc4c07a84ce3f02f7e81
kionys <kiwon7271@naver.com>
2024-09-28 21:01:39 +0900

    update: intro video 동영상 최적화 적용 및 주석추가

--- src/components/sections/video.tsx
+++ src/components/sections/video.tsx
@@ -14,6 +14,11 @@ const Video = () => {
         // controls={true}
         poster={'/assets/poster.jpg'}
       >
+        {/* 
+          source 태그 방식은 여러 확장자를 가진 영상을 대체제로 사용이 가능하도록합니다.
+          ex) mp4 영상을 사용하지못하는 브라우저이다? webm 형식의 영상으로 자동으로 대체해준다.
+        */}
+        <source src="/assets/main.webm" type="video/webm" />
         <source src="/assets/main.mp4" type="video/mp4" />
       </video>
     </Section>
